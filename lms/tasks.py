b9d91aca75a4effff870b03c553f064994169a7a
Ataraksia <1509lisavi@gmail.com>
2025-05-30 12:37:50 +0300

    fix flake8

--- lms/tasks.py
+++ lms/tasks.py
@@ -3,7 +3,7 @@ from datetime import timedelta, timezone
 from celery import shared_task
 from django.conf import settings
 from django.core.mail import send_mail
-from django.utils import timezone
+
 
 from lms.models import Course, Subscription
 from users.models import User
@@ -14,12 +14,10 @@ def course_update_mailing(course_pk):
     course = Course.objects.filter(pk=course_pk).first()
     users = User.objects.all()
     for user in users:
-        subscription = Subscription.objects.filter(
-            course=course_pk, user=user.pk
-        ).first()
+        subscription = Subscription.objects.filter(course=course_pk, user=user.pk).first()
         if subscription:
             send_mail(
-                subject=f"Обновление курса!",
+                subject="Обновление курса!",
                 message=f"Новые материалы по курсу '{course.name}'",
                 from_email=settings.EMAIL_HOST_USER,
                 recipient_list=[user.email],
@@ -43,8 +41,6 @@ def check_last_login():
         if timezone.now() - user.last_login > timedelta(days=30):
             user.is_active = False
             user.save()
-            print(
-                f"Пользователь {user.email} заблокирован, так как не заходил больше месяца"
-            )
+            print(f"Пользователь {user.email} заблокирован, так как не заходил больше месяца")
         else:
             print(f"Пользователь {user.email} активен")
